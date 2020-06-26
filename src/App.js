8bdcf86b3bc45d91bd7d3e0dae32b6fddf321bab
camcharp <cam.charpentier@live.fr>
2020-06-26 17:09:38 +0200

    set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/navigation/Header';
+import Bookslide from './components/alaune/Bookslide';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Bookslide/>
+    </div>
+  );
+}
+
+export default App;
